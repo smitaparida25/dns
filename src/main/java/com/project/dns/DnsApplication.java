8203152c0ca2503d3d853d0c0478cd6314ba2de4
Smita Parida <smitaparida182005@gmail.com>
2026-07-05 15:03:08 +0530

    feat: receive first UDP packet in DNS server

--- src/main/java/com/project/dns/DnsApplication.java
+++ src/main/java/com/project/dns/DnsApplication.java
@@ -1,13 +1,18 @@
 package com.project.dns;
 
+import com.project.dns.dns.DnsServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.SocketException;
 
 @SpringBootApplication
 public class DnsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DnsApplication.class, args);
+		DnsServer server = new DnsServer();
+		server.start();
 	}
-
 }
--- src/main/java/com/project/dns/dns/DnsServer.java
+++ src/main/java/com/project/dns/dns/DnsServer.java
@@ -1,5 +1,7 @@
 package com.project.dns.dns;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
@@ -9,5 +11,17 @@ public class DnsServer {
 
     public DnsServer() throws SocketException {
         datagramSocket = new DatagramSocket(8053);
+    }
+
+    // storage for the incoming datagram
+    private byte[] buffer = new byte[512];
+
+    // buffer only contains the data, we need to also store the additional info such as sender ip, sender address,length etc.
+    // we use datagram packet as the envelope that contains buffer and also the additional info
+
+    public void start() throws IOException {
+        DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+        datagramSocket.receive(datagramPacket);
+        System.out.println("Packet received!");
     }
 }
